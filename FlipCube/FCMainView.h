023171afa142730bff3b427c6da8558eb1badcc2
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2013-12-02 08:46:37 -0800

    Currently, no customization.

---
+++ FlipCube/FCMainView.h
@@ -0,0 +1,13 @@
+//
+//  FCMainView.h
+//  FlipCube
+//
+//  Created by daltman on 11/29/13.
+//  Copyright (c) 2013 Don Altman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCMainView : UIView
+
+@end
---
+++ FlipCube/FCMainView.m
@@ -0,0 +1,31 @@
+//
+//  FCMainView.m
+//  FlipCube
+//
+//  Created by daltman on 11/29/13.
+//  Copyright (c) 2013 Don Altman. All rights reserved.
+//
+
+#import "FCMainView.h"
+
+@implementation FCMainView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
