fc3219a72414da8cd9f74ffa3d141de0fad9dc66
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2013-12-02 16:39:29 -0800

    Maintain transforms on all faces (except back).

--- FlipCube/FCCubeView.m
+++ FlipCube/FCCubeView.m
@@ -59,13 +59,22 @@
 		self.flippingCubeLayerContainer.faceFront.hidden = NO;
 		self.flippingCubeLayerContainer.faceBack = [self addFace:@"chart2.png"];
 		self.flippingCubeLayerContainer.faceTop = [self addFace:@"chart3.png"];
+//		self.flippingCubeLayerContainer.faceTop.hidden = NO;
 		self.flippingCubeLayerContainer.faceBottom = [self addFace:@"chart4.png"];
+//		self.flippingCubeLayerContainer.faceBottom.hidden = NO;
 		self.flippingCubeLayerContainer.faceRight = [self addFace:@"chart5.png"];
 		self.flippingCubeLayerContainer.faceLeft = [self addFace:@"chart6.png"];
 		// perspective transform for container
 		CATransform3D transform = CATransform3DIdentity;
 		transform.m34 = -1.0/1000.0;
 		self.flippingCubeLayerContainer.sublayerTransform = transform;
+		// pre-transform faces to avoid startup artifacts
+		self.rotationDirection = FCVertical;
+		[self transformFace:self.flippingCubeLayerContainer.faceTop rotation:-M_PI_2];
+		[self transformFace:self.flippingCubeLayerContainer.faceBottom rotation:M_PI_2];
+		self.rotationDirection = FCHorizontal;
+		[self transformFace:self.flippingCubeLayerContainer.faceLeft rotation:-M_PI_2];
+		[self transformFace:self.flippingCubeLayerContainer.faceRight rotation:M_PI_2];
 	}
 	return  self;
 }
@@ -91,58 +100,21 @@
 	if (rotationInRadians > M_PI_2) rotationInRadians = M_PI_2;					// temporary
 	_rotationInRadians = rotationInRadians;
 	[self transformFace:self.flippingCubeLayerContainer.faceFront rotation:rotationInRadians];
-#if 0
-	float width = self.bounds.size.width/2.0;
-	float height = self.bounds.size.height/2.0;
-	float x = self.rotationDirection == FCHorizontal ? width*sinf(rotationInRadians) : 0;
-	float y = self.rotationDirection == FCVertical ? height*sinf(rotationInRadians) : 0;
-	float z = self.rotationDirection == FCHorizontal ? -(1.0 - sinf(rotationInRadians+M_PI_2))*height : -(1.0 - sinf(rotationInRadians+M_PI_2))*width;
-	// translate in 3D to reflect rotation about cube center
-	CATransform3D transformTranslated = CATransform3DTranslate(CATransform3DIdentity, x, y, z);
-	// rotate about horizontal or vertical axis
-	CATransform3D transform = self.rotationDirection == FCHorizontal ? CATransform3DRotate(transformTranslated, rotationInRadians, 0, 1, 0) : CATransform3DRotate(transformTranslated, -rotationInRadians, 1, 0, 0);
-	self.flippingCubeLayerContainer.faceFront.transform = transform;
-#endif
-	// deal with the lagging view
-	// difference from main face
-	if (rotationInRadians > 0)
-		rotationInRadians -= M_PI_2;
-	else
-		rotationInRadians += M_PI_2;
-#if 1
-	[self transformFace:self.flippingCubeLayerContainer.faceBottom rotation:rotationInRadians];
-#else
-	{
-		float x = self.rotationDirection == FCHorizontal ? width*sinf(rotationInRadians) : 0;
-		float y = self.rotationDirection == FCVertical ? height*sinf(rotationInRadians) : 0;
-		float z = self.rotationDirection == FCHorizontal ? -(1.0 - sinf(rotationInRadians+M_PI_2))*height : -(1.0 - sinf(rotationInRadians+M_PI_2))*width;
-		// translate in 3D to reflect rotation about cube center
-		CATransform3D transformTranslated = CATransform3DTranslate(CATransform3DIdentity, x, y, z);
-		// rotate about horizontal or vertical axis
-		CATransform3D transform = self.rotationDirection == FCHorizontal ? CATransform3DRotate(transformTranslated, rotationInRadians, 0, 1, 0) : CATransform3DRotate(transformTranslated, -rotationInRadians, 1, 0, 0);
-		self.flippingCubeLayerContainer.faceBottom.transform = transform;
-	}
-#if 0
-	// swap chart image on lagging face, depending on direction of movement
-	id contents;
-	if (self.rotationDirection == FCHorizontal) {
-		if (rotationInRadians > 0)
-			contents = (id) [UIImage imageNamed:@"chart2.png"].CGImage;
-		else
-			contents = (id) [UIImage imageNamed:@"chart3.png"].CGImage;
-	} else {
-		if (rotationInRadians > 0)
-			contents = (id) [UIImage imageNamed:@"chart4.png"].CGImage;
-		else
-			contents = (id) [UIImage imageNamed:@"chart5.png"].CGImage;
-	}
-	self.flippingCubeLayerContainer.faceBottom.contents = contents;
-#endif
-#endif
-	if (fabsf(rotationInRadians) >= 0.001)
+	if (self.rotationDirection == FCVertical) {
+		[self transformFace:self.flippingCubeLayerContainer.faceTop rotation:rotationInRadians-M_PI_2];
+		[self transformFace:self.flippingCubeLayerContainer.faceBottom rotation:rotationInRadians+M_PI_2];
+		self.flippingCubeLayerContainer.faceLeft.hidden = YES;
+		self.flippingCubeLayerContainer.faceRight.hidden = YES;
+		self.flippingCubeLayerContainer.faceTop.hidden = NO;
 		self.flippingCubeLayerContainer.faceBottom.hidden = NO;
-	else
+	} else {
+		[self transformFace:self.flippingCubeLayerContainer.faceLeft rotation:rotationInRadians-M_PI_2];
+		[self transformFace:self.flippingCubeLayerContainer.faceRight rotation:rotationInRadians+M_PI_2];
+		self.flippingCubeLayerContainer.faceLeft.hidden = NO;
+		self.flippingCubeLayerContainer.faceRight.hidden = NO;
+		self.flippingCubeLayerContainer.faceTop.hidden = YES;
 		self.flippingCubeLayerContainer.faceBottom.hidden = YES;
+	}
 }
 
 - (float)rotationInRadians
