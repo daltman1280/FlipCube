94cc86a9c8f251e2d0461099fe452474b4030178
daltman <daltman@Don-Altmans-MacBook-Pro.local>
2013-12-03 23:42:48 -0800

    Avoid header recursion.

---
+++ FlipCube/FCDirection.h
@@ -0,0 +1,17 @@
+//
+//  FCDirection.h
+//  FlipCube
+//
+//  Created by daltman on 12/2/13.
+//  Copyright (c) 2013 Don Altman. All rights reserved.
+//
+
+#ifndef FlipCube_FCDirection_h
+#define FlipCube_FCDirection_h
+
+typedef enum {
+	FCHorizontal,
+	FCVertical
+} FCDirection;
+
+#endif
